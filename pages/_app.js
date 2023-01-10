971619bd6b1937b142de0491f3314f9b6bb0ebaf
mohammedahmed18 <mohammed18200118@gmail.com>
2023-01-10 21:18:42 +0200

    styling initialization
    - added tailwindcss
    - added themes folder (light , dark) and apply the light theme to the ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
+import { useEffect } from 'react'
 import '../styles/globals.css'
+// load the theme
+import lightTheme from '../themes/light.json'
 
 export default function App({ Component, pageProps }) {
+
+  useEffect(() => {
+    // TODO: move this logic to redux actions and add theme switch between light and dark
+    // apply the theme to the ui
+    const root = document.documentElement;
+    Object.entries(lightTheme).forEach(([property, value]) => {
+      root.style.setProperty(property, value);
+    });
+  } , [])
+
   return <Component {...pageProps} />
 }
