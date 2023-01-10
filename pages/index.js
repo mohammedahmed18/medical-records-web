2f3ba0a04e2737328f1e8ec14ebb1895e1246ce8
mohammedahmed18 <mohammed18200118@gmail.com>
2023-01-10 20:44:51 +0200

    test vercel deployment

--- pages/index.js
+++ pages/index.js
@@ -17,8 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
+           Hello from next js
           </p>
           <div>
             <a
