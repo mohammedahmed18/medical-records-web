bafa326d1b4d8323d16148bfccf961d034d39f5a
mohammedahmed18 <mohammed18200118@gmail.com>
2023-03-03 17:49:32 +0200

    change greeting message

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { AccountBox } from "@/components/accountBox";
 export default function Home() {
   return (
     <>
-      <h1>hello world 1</h1>
+      <h1>hello Next js</h1>
       <AccountBox />
     </>
   )
