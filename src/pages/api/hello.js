6d49d22e4eaae7b8d22e956a4540df1cff5be3c7
mohammedahmed18 <mohammed18200118@gmail.com>
2023-03-03 17:16:41 +0200

    changes

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'hello John Doe' })
+  res.status(200).json({ name: 'hello John Doe safsa' })
 }
