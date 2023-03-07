7678f137670478757a06dde452386d5e27d7881e
mohammedahmed18 <mohammed18200118@gmail.com>
2023-03-07 17:50:12 +0200

    refactor: add og image

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -3,19 +3,17 @@ import { useRouter } from 'next/router';
 
 // !STARTERCONF Change these default meta
 const defaultMeta = {
-  title: 'Next.js + Tailwind CSS + TypeScript Starter',
-  siteName: 'Next.js + Tailwind CSS + TypeScript Starter',
-  description:
-    'A starter for Next.js, Tailwind CSS, and TypeScript with Absolute Import, Seo, Link component, pre-configured with Husky',
-  /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  url: 'https://tsnext-tw.thcl.dev',
+  title: 'Medical Records',
+  siteName: 'Medical Records',
+  description: 'medical records tracking app  ',
+  url: 'https://medical-records-web.vercel.app',
   type: 'website',
   robots: 'follow, index',
   /**
    * No need to be filled, will be populated with openGraph function
    * If you wish to use a normal image, just specify the path below
    */
-  image: 'https://tsnext-tw.thcl.dev/images/large-og.png',
+  image: 'https://tsnext-tw.thcl.dev/images/medical-records-og.png',
 };
 
 type SeoProps = {
@@ -29,12 +27,12 @@ export default function Seo(props: SeoProps) {
     ...defaultMeta,
     ...props,
   };
+  // Use siteName if there is templateTitle
+  // but show full title if there is none
   meta['title'] = props.templateTitle
     ? `${props.templateTitle} | ${meta.siteName}`
     : meta.title;
 
-  // Use siteName if there is templateTitle
-  // but show full title if there is none
   // !STARTERCONF Follow config for opengraph, by deploying one on https://github.com/theodorusclarence/og
   // ? Uncomment code below if you want to use default open graph
   // meta['image'] = openGraph({
@@ -56,13 +54,6 @@ export default function Seo(props: SeoProps) {
       <meta property='og:description' content={meta.description} />
       <meta property='og:title' content={meta.title} />
       <meta name='image' property='og:image' content={meta.image} />
-      {/* Twitter */}
-      <meta name='twitter:card' content='summary_large_image' />
-      {/* // !STARTERCONF Remove or change to your handle */}
-      {/* <meta name='twitter:site' content='@th_clarence' /> */}
-      <meta name='twitter:title' content={meta.title} />
-      <meta name='twitter:description' content={meta.description} />
-      <meta name='twitter:image' content={meta.image} />
       {meta.date && (
         <>
           <meta property='article:published_time' content={meta.date} />
@@ -70,12 +61,6 @@ export default function Seo(props: SeoProps) {
             name='publish_date'
             property='og:publish_date'
             content={meta.date}
-          />
-          {/* // !STARTERCONF Remove or change to your name */}
-          <meta
-            name='author'
-            property='article:author'
-            content='Theodorus Clarence'
           />
         </>
       )}
---
+++ src/pages/login.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+import Layout from '@/components/layout/Layout';
+import Seo from '@/components/Seo';
+
+export default function LoginPage() {
+  return (
+    <Layout>
+      <Seo templateTitle='Login' />
+
+      <main>
+        <section className=''>
+          <div className='layout'>hello</div>
+        </section>
+      </main>
+    </Layout>
+  );
+}
