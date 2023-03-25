75e0bd0f9c7b6efbcdb8ea19c86f863af749646a
mohammedahmed18 <mohammed18200118@gmail.com>
2023-03-25 18:00:06 +0200

    fix: move the navnar to the _app file so that the qr code doesn't get removed when changing the url

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,12 +1,9 @@
 import * as React from 'react';
 
-import Navbar from '@/components/navbar';
-
 export default function Layout({ children }: { children: React.ReactNode }) {
   // Put Header or Footer Here
   return (
     <>
-      <Navbar />
       <div className='mt-10'>{children}</div>
     </>
   );
