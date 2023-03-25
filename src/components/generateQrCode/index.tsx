5495aa666e2e07d82d89099be54fffb8a54341ca
mohammedahmed18 <mohammed18200118@gmail.com>
2023-03-25 14:30:53 +0200

    style: modal design

--- src/components/generateQrCode/index.tsx
+++ src/components/generateQrCode/index.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import React, { useEffect, useState } from 'react';
 import QRCode from 'react-qr-code';
 import { useQuery } from 'react-query';
@@ -7,6 +8,7 @@ import { useTimeoutAsync } from '@/hooks/useTimeoutAsync';
 import IconButton from '@/components/buttons/IconButton';
 import Modal from '@/components/common/modal';
 import Spinner from '@/components/common/spinner';
+import Tooltip from '@/components/common/tooltip';
 
 import { generateQrcode } from '@/api/users';
 import { showToast } from '@/utils/toast';
@@ -28,13 +30,13 @@ const GenerateQrCode: React.FC = () => {
     data: qrcode,
     isLoading: loading,
     refetch,
+    isRefetching,
   } = useQuery({
     queryFn: generateQrcode,
     enabled: false,
   });
 
   const handleGenerateQrcode = () => {
-    setShowQrcode(true);
     if (!canRefresh) {
       return showToast(
         `you must wait ${QR_LIFETIME_MINS} minute(s) to refresh the qr code`,
@@ -44,6 +46,10 @@ const GenerateQrCode: React.FC = () => {
     setCanRefresh(false);
     refetch();
   };
+  const handleShowQrModal = () => {
+    setShowQrcode(true);
+    if (!qrcode) handleGenerateQrcode();
+  };
 
   useEffect(() => {
     if (!canRefresh) set();
@@ -51,26 +57,34 @@ const GenerateQrCode: React.FC = () => {
 
   return (
     <>
-      <button onClick={handleGenerateQrcode}>
+      <button onClick={handleShowQrModal}>
         <QrcodeIcon className='text-6xl transition-transform duration-300 hover:-translate-y-2' />
       </button>
 
       <Modal shown={showQrcode} onClose={() => setShowQrcode(false)}>
         <div className='flex flex-col items-center justify-center gap-4'>
-          <IconButton
-            onClick={handleGenerateQrcode}
-            icon={RefreshIcon}
-            variant='light'
-            className='text-2xl'
-          />
-          {loading && <Spinner size={70} color='primary-100' />}
-          {qrcode && (
-            <QRCode
-              value={qrcode}
-              size={200}
-              className='rounded-2xl p-4 ring-4 ring-primary-200'
+          <Tooltip title='refresh'>
+            <IconButton
+              onClick={handleGenerateQrcode}
+              icon={RefreshIcon}
+              variant='light'
+              className={clsx(
+                'rounded-full text-2xl',
+                isRefetching ? 'animate-spin' : ''
+              )}
             />
-          )}
+          </Tooltip>
+
+          <div className='center-content min-h-[200px]'>
+            {loading && <Spinner size={70} color='primary-100' />}
+            {qrcode && (
+              <QRCode
+                value={qrcode}
+                size={200}
+                className='rounded-2xl p-4 ring-4 ring-primary-200'
+              />
+            )}
+          </div>
         </div>
       </Modal>
     </>
