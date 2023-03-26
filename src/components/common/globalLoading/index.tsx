3ab9b63ff353bc625d51b42d58cbc2ff36845a80
mohammedahmed18 <mohammed18200118@gmail.com>
2023-03-26 11:37:38 +0200

    fix: hide the navbar when the user is not authenticated and style the global loading spinner

--- src/components/common/globalLoading/index.tsx
+++ src/components/common/globalLoading/index.tsx
@@ -2,8 +2,8 @@ import Spinner from '@/components/common/spinner';
 
 const GlobalLoading = () => {
   return (
-    <div className='center-content h-screen'>
-      <Spinner size={70} color='blue-500' />
+    <div className='center-content fixed inset-0 z-[99999] h-screen'>
+      <Spinner size={70} className='border-primary-50' />
     </div>
   );
 };
