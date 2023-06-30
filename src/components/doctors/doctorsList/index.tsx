9d591068355ca2f56f061a2a6bc69581b368ae1d
mohammedahmed18 <mohammed18200118@gmail.com>
2023-06-30 23:25:45 +0300

    style: no doctors message

--- src/components/doctors/doctorsList/index.tsx
+++ src/components/doctors/doctorsList/index.tsx
@@ -16,6 +16,27 @@ const DoctorsList = () => {
     queryFn: () => getAllDoctors(searchParmas),
   });
 
+  const Content = () => {
+    if (!isLoading && !results?.length) {
+      return (
+        <div className='center-content w-full'>
+          <h2 className='text-muted text-6xl font-light tracking-wider'>
+            No Doctors found
+          </h2>
+        </div>
+      );
+    } else if (isLoading) {
+      return <DoctorsSkeleton />;
+    } else {
+      return (
+        <div className='grid h-fit w-full grid-cols-1 gap-5 p-4 md:grid-cols-2 lg:grid-cols-3'>
+          {results?.map((doctor) => (
+            <DoctorSearchCard key={doctor.id} doctor={doctor} />
+          ))}
+        </div>
+      );
+    }
+  };
   return (
     <div className='flex flex-col gap-3 md:flex-row'>
       <div className='w-full md:w-1/2 lg:w-1/3'>
@@ -26,15 +47,8 @@ const DoctorsList = () => {
           clearFilters={() => setSearchParams({})}
         />
       </div>
-      {isLoading ? (
-        <DoctorsSkeleton />
-      ) : (
-        <div className='grid h-fit w-full grid-cols-1 gap-5 p-4 md:grid-cols-2 lg:grid-cols-3'>
-          {results?.map((doctor) => (
-            <DoctorSearchCard key={doctor.id} doctor={doctor} />
-          ))}
-        </div>
-      )}
+
+      {Content()}
     </div>
   );
 };
