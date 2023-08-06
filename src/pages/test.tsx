216b1013011b29340ba33d2a21bfd3ef40b348db
mohammedahmed18 <mohammed18200118@gmail.com>
2023-08-06 08:15:20 +0300

    style: test rapiddata package

---
+++ src/pages/test.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@rapiddata-ai/design-system';
+
+import Layout from '@/components/layout';
+const TestPage = () => {
+  return (
+    <Layout>
+      <Button>Click me</Button>;
+    </Layout>
+  );
+};
+
+export default TestPage;
